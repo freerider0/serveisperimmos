accc868307e349a2e96095f71256458cd9ba7c8b
Rumech <rumech@gmail.com>
2024-10-02 19:14:11 +0200

    added first page draft quietes fixed

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -82,7 +82,7 @@ export function LandingPageComponent() {
                   <ul className="list-disc pl-6">
                     <li>Vas a quedar muy bien con el propietario.</li>
                     <li>Te vas a ahorrar un montón de visitas improductivas.</li>
-                    <li>El propietario estará contento porque ve que llevas a gente que como mínimo tiene cierto interés y no hace una de esas visitas que solo pasar por la puerta dicen "uyyy no".</li>
+                    <li>El propietario estará contento porque ve que llevas a gente que como mínimo tiene cierto interés y no hace una de esas visitas que solo pasar por la puerta dicen '"uyyy no"'.</li>
                   </ul>
                 </>
               }
